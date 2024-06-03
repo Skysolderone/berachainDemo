ee29c87ae49c3a5dbe834188bf865efa3156f452
wws <wws741@qq.com>
2024-06-03 12:12:19 +0800

    0603

--- erc20token/contracts/mytoken.sol
+++ erc20token/contracts/mytoken.sol
@@ -6,8 +6,8 @@ import "hardhat/console.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    uint256 public constant INIT_TOTAL = 1000_000;
-    constructor() ERC20("Test", "TS") {
+    uint256 public constant INIT_TOTAL = 1000_000 * 1 ether;
+    constructor() ERC20("WsToken", "WS") {
         _mint(msg.sender, INIT_TOTAL);
         console.log("%s", msg.sender);
     }
@@ -15,3 +15,4 @@ contract MyToken is ERC20 {
     //     return ERC20(address(this)).balanceOf();
     // }
 }
+//0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9
--- erc20token/scripts/deploy.ts
+++ erc20token/scripts/deploy.ts
@@ -1,19 +1,38 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-  const mytoken = await ethers.deployContract('MyToken');
-  mytoken.waitForDeployment();
-  console.log(`mytoken is ${mytoken.target}`);
+  // const mytoken = await ethers.deployContract('MyToken');
+  // mytoken.waitForDeployment();
+  // console.log(`mytoken is ${mytoken.target}`);
   //   const ownAddress = mytoken.target;
+  // const provider = new ethers.JsonRpcProvider('http:127.0.0.1:8545/');
+  const mytoken = await ethers.getContractAt(
+    'MyToken',
+    '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9'
+  );
+  // const singa = new ethers.Wallet(
+  //   '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
+  //   provider
+  // );
+  // console.log(`singa is ${singa.address}`);
   const balance = await mytoken.balanceOf(
     '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+    // '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199'
   );
   console.log(`balance of is ${balance}`);
-  mytoken.transferFrom(
-    '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+  // const allowvalue = ethers.parseUnits('100', 18);
+  // const allowtx = await mytoken.approve(
+  //   '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+  //   allowvalue
+  // );
+  // await allowtx.wait();
+  const value = ethers.parseUnits('10', 18);
+  const tx = await mytoken.transfer(
+    //'0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
     '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
-    10
+    value
   );
+  await tx.wait();
   const balance2 = await mytoken.balanceOf(
     '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'
   );
@@ -24,3 +43,9 @@ main().catch((error) => {
   console.log(error);
   process.exitCode = 1;
 });
+
+// Account #0: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 (10000 ETH)
+// Private Key: 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+// Account #1: 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 (10000 ETH)
+// Private Key: 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d
